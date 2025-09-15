c0c6d9534ba86788213e2cb31bcc4f1c560fe90e
Alireza Pourreza <alirezapourreza@Alirezas-MacBook-Pro.local>
2025-09-15 19:50:23 -0400

    added Polynomial.java and Driver.java

---
+++ Polynomial.java
@@ -0,0 +1,38 @@
+public class Polynomial{
+    double[] coefficients;
+    
+    public Polynomial(){
+        this.coefficients = new double[]{0.0};
+    }
+
+    public Polynomial(double[] coefficients){
+        this.coefficients = coefficients;
+    }
+
+    public Polynomial add(Polynomial other){
+        int mx = Math.max(this.coefficients.length, other.coefficients.length);
+        double[] res = new double[mx];
+
+        for(int i = 0; i < mx; i++){
+            res[i] = 0.0;
+            if(i < other.coefficients.length) res[i] += other.coefficients[i];
+            if(i < this.coefficients.length) res[i] += this.coefficients[i];
+        }
+        
+        return new Polynomial(res);
+    }
+
+    public double evaluate(double x){
+        double res = 0.0;
+        double curr = 1.0;
+        for(int i = 0; i < this.coefficients.length; i++){
+            res += this.coefficients[i] * curr;
+            curr *= x;
+        }
+        return res;
+    }
+
+    public boolean hasRoot(double x){
+        return evaluate(x) == 0.0;
+    }
+}
